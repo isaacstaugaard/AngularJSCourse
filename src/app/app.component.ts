66edd1e32e17e9f892002e244bde64961aca7156
isaacstaugaard <isaacestaugaard@gmail.com>
2018-09-18 22:50:43 -0700

    Section 2 Lecture 29

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,11 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
+  //styles: [`
+  //	h3 { 
+  //		color: purple
+  //	}
+  //`]
 })
 export class AppComponent {
-  title = 'my-first-app';
 }
